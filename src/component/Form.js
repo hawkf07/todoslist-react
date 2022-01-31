102098718c6054ee7cca58585dd25b33a3e5d266
fikri <fikrifadilah299@gmail.com>
2022-01-31 20:45:17 +0700

    create form component

--- src/component/Form.js
+++ src/component/Form.js
@@ -1,15 +1,14 @@
 import {useState,useEffect} from 'react'
 
-const Form = () => {
-  const formHandler = (e) => {
-    e.preventDefault()
-
+const Form = ({userInput,setUserInput,formHandler}) => {
+  const userInputHandler = (e) => {
+    setUserInput(e.target.value)
   }
   
   return (
-    <form className="p-2" onClick={formHandler}>
+    <form className="formInput d-flex py-3 align-items-center justify-content-center" onSubmit={formHandler}>
       <div>
-      <input className="input-form" placeholder="please input todos"  />
+      <input value={userInput} onChange={userInputHandler} className="input-form" placeholder="please input todos"  />
         <button className='btn-form-submit bg-warning'>+</button>
       </div>
     </form>
