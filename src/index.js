6ab43107cedc6cb91feb29aeed3b8560e986391d
fikri <fikrifadilah299@gmail.com>
2022-01-31 20:39:46 +0700

    adding bootstrap css and js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
